f3349022635a54b4be2deb81032a7457bec9d6f9
GODABA <abayomgodwin27@gmail.com>
2025-03-29 04:35:20 +0000

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
 import App1 from './App1';
 import App2 from './App2';
 
 function App(){
  
   return(
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<App1 />} />
         <Route path="/Message" element={<App2 />}/>
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 export default App;
