7bde4c9e46af04b8c223aab06b60168beba47c1f
GODABA <abayomgodwin27@gmail.com>
2025-07-30 21:06:53 +0000

    ''

--- src/MyComponent1.jsx
+++ src/MyComponent1.jsx
@@ -15,17 +15,10 @@ function MyComponent1(){
             <div className="nav-bar">
                 <div className="text-logo">G-Acrypt</div>
                 <div className="menu">   
-                    <a href="#knowMore">Home</a>
-                    <a href="#myWorks">Projects</a>
-                    <a href="#experience">Experience</a>  
+
                 </div> 
                 <Link to="/Message" className='contactUs'>contactMe</Link>
             </div>
-            <div className={`mobile-menu ${isMenuOpen ? 'open' : ''}`}>
-                    <Link to="/#knowMore" onClick={toggleMenu}>Home</Link>
-                    <a href="/#myWorks" onClick={toggleMenu}>Projects</a>
-                    <Link to="/#experience" onClick={toggleMenu}>Experience</Link>
-                </div>
             </div>
     );
 }
