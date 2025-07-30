541f864f84fa604e7da389d2886b0f4f282ee065
GODABA <abayomgodwin27@gmail.com>
2025-07-30 21:41:15 +0000

    ''

--- src/Message.jsx
+++ src/Message.jsx
@@ -7,19 +7,19 @@ function Message(){
             <form className="form1" >
                 <div className="head1">Talk to me</div>
                 <div className="formInput1">
-                    <label htmlFor="email"> Email: </label> <br />
+                    <label htmlFor="email" className='messageLabel'> Email: </label> <br />
                     <input type="text" name="email" placeholder="Enter your email" className="input1"/>
                 </div>
                 <div className="formInput1">
-                    <label htmlFor="name"> Name: </label> <br />
+                    <label htmlFor="name" className='messageLabel'> Name: </label> <br />
                     <input type="text" name="name" placeholder="Enter your name" className="input1"/>
                 </div>
                 <div className="formInput1">
-                    <label htmlFor="tel"> Phone Number: </label> <br />
+                    <label htmlFor="tel" className='messageLabel'> Phone Number: </label> <br />
                     <input type="tel" name="tel" placeholder="Enter your phone number" className="input1"/>
                 </div>
                 <div className="formInput1">
-                    <label htmlFor="message"> Message: </label> <br />
+                    <label htmlFor="message" className='messageLabel'> Message: </label> <br />
                     <textarea name="message" id="message" rows="10" placeholder="Type your message" className='textArea1' ></textarea>
                 </div>
                 <div className="formInput1"> 
